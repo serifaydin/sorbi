97d22f35d19b0aa2a7a8b70726dcacfe1acb1fd7
Serif Aydin <serifaydin99@gmail.com>
2020-02-09 22:57:22 +0300

    sorbi upload
    sorbi upload

---
+++ Sorbi/Sorbi/Bots/EchoBot.cs
@@ -0,0 +1,96 @@
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using ServiceStack.Support.Markdown;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorbi.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        public static string ordernumber = "";
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = turnContext.Activity.Text;
+
+            if (replyText.Contains("sipariþ"))
+            {
+                await SendSuggestedActionsPlatformAsync(turnContext, cancellationToken);
+                return;
+            }
+
+            if (replyText == "PX")
+            {
+                await SendSuggestedActionsAccountAsync(turnContext, cancellationToken);
+                return;
+            }
+
+            if (replyText == "IKEA")
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Lütfen Sipariþ Nunamasý giriniz."), cancellationToken);
+                return;
+            }
+
+            if (replyText == "123456789")
+            {
+                await SendSuggestedActionPackingAsync(turnContext, cancellationToken);
+                ordernumber = replyText;
+                return;
+            }
+
+            if (replyText == "6")
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(ordernumber + " numaralý sipariþ için lütfen karþýlaþtýðýnýz sorunu yazýnýz"), cancellationToken);
+                return;
+            }
+
+            //await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Merhaba ben Sorbi, Size nasýl yardýmcý olabilirim.";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+
+        private static async Task SendSuggestedActionsPlatformAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("Size daha iyi yardýmcý olabilmem için, Lütfen Depo seçiniz.");
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = CardActionManager.GetPlatforms()
+            };
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        private static async Task SendSuggestedActionsAccountAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("Lütfen Müþteri seçiniz.");
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = CardActionManager.GetAccountsc()
+            };
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        private static async Task SendSuggestedActionPackingAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text(ordernumber + " numaralý sipariþ için, Aþaðýdaki hangi nedeni söyleyebilirsiniz.");
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = CardActionManager.GetPackingErrors()
+            };
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+    }
+}
---
+++ Sorbi/Sorbi/CardActionManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+
+namespace Sorbi
+{
+    public static class CardActionManager
+    {
+        public static List<CardAction> GetPlatforms()
+        {
+            return new List<CardAction>
+            {
+                new CardAction() { Title = "Platform X", Type = ActionTypes.ImBack, Value = "PX" },
+                new CardAction() { Title = "Platform S", Type = ActionTypes.ImBack, Value = "PS" },
+                new CardAction() { Title = "Platform S2", Type = ActionTypes.ImBack, Value = "PS2" },
+                new CardAction() { Title = "Platform M", Type = ActionTypes.ImBack, Value = "PM" },
+                new CardAction() { Title = "Platform M2", Type = ActionTypes.ImBack, Value = "PM2" },
+                new CardAction() { Title = "Platform L", Type = ActionTypes.ImBack, Value = "PL" }
+            };
+        }
+
+        public static List<CardAction> GetAccountsc()
+        {
+            return new List<CardAction>
+            {
+                new CardAction() { Title = "IKEA", Type = ActionTypes.ImBack, Value = "IKEA" },
+                new CardAction() { Title = "AYPIMA", Type = ActionTypes.ImBack, Value = "AYPIMA" }
+            };
+        }
+
+        public static List<CardAction> GetPackingErrors()
+        {
+            return new List<CardAction>
+            {
+                new CardAction() { Title = "Ortak Etiket Sorunu", Type = ActionTypes.ImBack, Value = "1" },
+                new CardAction() { Title = "Arvato Etiketi Sorunu", Type = ActionTypes.ImBack, Value = "2" },
+                new CardAction() { Title = "Görev Bulunamadı sorunu", Type = ActionTypes.ImBack, Value = "3" },
+                new CardAction() { Title = "İrsaliye sorunu", Type = ActionTypes.ImBack, Value = "4" },
+                new CardAction() { Title = "İptal Sipariş Transfer sorunu", Type = ActionTypes.ImBack, Value = "5" },
+                new CardAction() { Title = "Diğer", Type = ActionTypes.ImBack, Value = "6" },
+            };
+        }
+    }
+}
